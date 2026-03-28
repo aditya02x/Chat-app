11f4153b86d8d33cd9e75561e5da9d4a7fbcdcef
Aditya Raj <adityaraj34@icloud.com>
2026-03-29 00:38:14 +0530

    middle ware and generate token

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,18 +1,19 @@
 import User from '../models/User.model.js'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
+import generatetoken from '../utils/generateToken.js'
 
 export const signup = async (req,res)=>{
     try {
         const {fullname,username,password}=req.body
 
         if(!fullname || !username || !password){
-            return res.status(404).json({menubar:"all credintial are required"})
+            return res.status(404).json({message:"all credintial are required"})
         }
 
         const existUser = await User.findOne({username})
 
         if(existUser){
-            return res.status(400).json({mesage:"user allready exist"})
+            return res.status(400).json({message:"user allready exist"})
         }
 
         //hash password
@@ -27,20 +28,60 @@ export const signup = async (req,res)=>{
         })
        
         await user.save()
-        res.status(201).json({ message: "User created successfully" })
+        const token = generatetoken(user._id)
+        res.status(201).json({ message: "User created successfully" ,
+             user: {
+        _id: user._id,
+        fullname: user.fullname,
+        username: user.username,
+        profilePic: user.profilePic
+    },
+                token
+         })
 
 
         
     } catch (error) {
         console.error(error)
-        return res.staus(500).json({mesage:"Server error"})
+        return res.status(500).json({mesage:"Server error"})
         
     }
 }
 export const login =async(req,res)=>{
     try {
+        const {username,password} = req.body
+
+        if(!username || !password){
+             return res.status(404).json({menubar:"all credintial are required"})
+        }
+
+      const user = await User.findOne({username})  
+
+      if(!user){
+           return res.status(400).json({mesage:"user doesnot exist"})
+      }
+
+      const isMatch = await bcrypt.compare(password,user.password)
+
+      if(!isMatch){
+        return res.status(400).json({message:"INVALID CREDINTIAL"})
+      }
+      const token = generatetoken(user._id)
+
+              res.status(201).json({ message: "login sucessfully",
+                 user: {
+        _id: user._id,
+        fullname: user.fullname,
+        username: user.username,
+        profilePic: user.profilePic
+    },
+                token
+               })
+
+      
         
     } catch (error) {
-        
+         console.error(error)
+        return res.status(500).json({message:"Server error"})
     }
 }
