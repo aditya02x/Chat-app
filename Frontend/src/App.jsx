1fac0c1bd4506ee60f7cc9cb644f11e87012d931
Aditya Raj <adityaraj34@icloud.com>
2026-03-29 23:28:27 +0530

    route for frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   )
 }
